b36ead066eab7eec5252562bc0e6d44c456b1b0e
Alex Breuer <alex.breuer@stjude.org>
2019-10-29 10:17:09 -0500

    results I showed on Friday

--- Min4data_run.py
+++ Min4data_run.py
@@ -10,7 +10,7 @@ LysConc = 200
 
 n = 1e5
 
-t = np.hstack((np.array(f['td']),[32.]))
+t = nr.uniform(0,32,2048)
 t.sort()
 f = me.ft(Lambda,C,t).T
 df = me.dft(Lambda,C,t).T
@@ -20,7 +20,7 @@ ThetaH = np.hstack([f[:,0].reshape((-1,1)) - f[:,i].reshape((-1,1))
 ThetaA = .05 - f[:,0]
 
 
-A,b = de.lstSqMultiPt( ThetaH[:,:], ThetaA.reshape((-1,1)), df[:,1:], df[:,0].reshape((-1,1)), etaP.reshape((-1,))/206, t, .0 )[:2]
+A,b = de.lstSqMultiPt( ThetaH[:,:], ThetaA.reshape((-1,1)), df[:,1:], df[:,0].reshape((-1,1)), etaP.reshape((-1,))/206, t, .0125 )[:2]
 B = de.lppBoltOn(ThetaH.shape[0],A.shape[1],t,.0)
 M = ss.hstack((A,B)).tocsr()
 G = (M.T*M).todense()
@@ -28,11 +28,11 @@ U,S,Vt = nl.svd(G)
 gammaUt = Vt[:-1,:].T*(np.diagflat(S[:-1]/(S[:-1]**2+0))*(U[:,:-1].T*(M.T*b)))
 MTb = np.array(M.T*b).reshape((-1,))
 G = np.array(G)
-res = so.minimize( lambda x: nl.norm(np.dot(G,x) - MTb)**2, np.array(np.abs(gammaUt)).reshape((-1,)), jac=lambda x: np.dot(G,x) - MTb, method='SLSQP', options={'iprint':3,'maxiter':64}, bounds=[(0,None)]*G.shape[0] )
+#res = so.minimize( lambda x: nl.norm(np.dot(G,x) - MTb)**2, np.array(np.abs(gammaUt)).reshape((-1,)), jac=lambda x: np.dot(G,x) - MTb, method='SLSQP', options={'iprint':3,'maxiter':64}, bounds=*[(0,None)]*G.shape[0] )
 
-reg = co.coupledOde( Lambda.shape[1]-1, etaP.reshape((-1,)), 206, np.ones(Lambda.shape[1]-1), np.ones(1), np.ones(1)*.05, np.array(res.x[:A.shape[1]]).reshape((-1,)) )
+reg = co.coupledOde( Lambda.shape[1]-1, etaP.reshape((-1,)), 206, np.ones(Lambda.shape[1]-1), np.ones(1), np.ones(1)*.05, np.array(gammaUt[:A.shape[1]]).reshape((-1,)) )
 sys = control.ss(reg.A().detach(),np.eye(reg.A().shape[0]),np.eye(reg.A().shape[0]),np.zeros(reg.A().shape))
-ut = np.vstack(([[0]],np.array(res.x[A.shape[1]:]).reshape((-1,1))))
+ut = np.vstack(([[0]],np.array(gammaUt[A.shape[1]:]).reshape((-1,1))))
 tp = np.hstack(([0],t))
 Uv = np.zeros((reg.A().shape[0],641))
 Uv[-2,:] = si.UnivariateSpline(tp,ut.reshape((-1,)),s=10)(np.arange(0,32+5e-2,5e-2))
